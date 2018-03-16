954dec0e347763c7df293c4124ad35bd309039d5
MontasserBH <montasserb94@gmail.com>
2018-03-16 10:48:31 +0100

    Communication entre StartActivity et GameActivity + insertion de nom dans la firebase

--- app/src/main/java/com/m2dl/mobebmp/mobe_catapulte/StartActivity.java
+++ app/src/main/java/com/m2dl/mobebmp/mobe_catapulte/StartActivity.java
@@ -1,13 +1,38 @@
 package com.m2dl.mobebmp.mobe_catapulte;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StartActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        final EditText name = findViewById(R.id.editText3);
+        Button buttonStart = findViewById(R.id.button_start);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Utilisateur utilisateur = new Utilisateur(name.toString(), 0);
+                databaseReference.child(name.toString()).setValue(utilisateur);
+                Intent intent = new Intent(StartActivity.this, ScoreActivity.class);
+                intent.putExtra("nameFromStartActivity", name.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
