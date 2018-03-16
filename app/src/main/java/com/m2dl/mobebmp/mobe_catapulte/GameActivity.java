6b69de550cbbb6e365aeaf54b8f111b109c18336
franckpicardat <franck-ten@hotmail.fr>
2018-03-16 16:48:42 +0100

    Ajout des bonus + gestion collision

--- app/src/main/java/com/m2dl/mobebmp/mobe_catapulte/GameActivity.java
+++ app/src/main/java/com/m2dl/mobebmp/mobe_catapulte/GameActivity.java
@@ -1,21 +1,34 @@
 package com.m2dl.mobebmp.mobe_catapulte;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorListener;
 import android.hardware.SensorManager;
 import android.os.CountDownTimer;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
     private  String score_name;
@@ -63,14 +76,20 @@ public class GameActivity extends AppCompatActivity {
 
     private boolean finish = false;
     private long startTime = System.currentTimeMillis();
-    private final long timeLimit = 9000L; // Limit to 10 hours
+    private long startTimeBonus = System.currentTimeMillis();
+    private final long timeLimit = 9000L;
+    private final long timeLimitBonus = 900L; // Limit to 10 hours
     private CountDownTimer timer;
+    private CountDownTimer timerBonus;
+    private List<Bonus> bonusList;
+    private Context context;
 
     private TranslateAnimation moveLefttoRight = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 400.0f, Animation.RELATIVE_TO_SELF, -700.0f);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        context = this;
         if(getIntent() != null && getIntent().getExtras() != null){
             score_name = getIntent().getExtras().getString("nameFromScoreActivity");
             start_name = getIntent().getExtras().getString("nameFromStartActivity");
@@ -95,19 +114,64 @@ public class GameActivity extends AppCompatActivity {
         timer = new CountDownTimer(timeLimit, 61) {
             public void onTick(long r) {
                 // Running
-                    long millis = System.currentTimeMillis() - startTime;
-                    String millisStr = String.valueOf(millis);
+                long millis = System.currentTimeMillis() - startTime;
+                String millisStr = String.valueOf(millis);
 
-                    // Display
-                    if (millisStr.length() > 3) {
-                        String time = String.valueOf(millis / 1000) + "." + millisStr.substring(millisStr.length() - 2);
-                    }
+                // Display
+                if (millisStr.length() > 3) {
+                    String time = String.valueOf(millis / 1000) + "." + millisStr.substring(millisStr.length() - 2);
+                }
             }
 
             public void onFinish() {
                 finish = true;
             }
         }.start();
+
+        timerBonus = new CountDownTimer(timeLimitBonus, 61) {
+            public void onTick(long r) {
+                ImageView flyingman = findViewById(R.id.flyingman);
+
+                for (Bonus bonus : bonusList)
+                {
+                    ImageView bonusView = findViewById(bonus.getId());
+                    if (collision(flyingman, bonusView))
+                    {
+                        // Ajouter du temps ou augmenter score
+
+                        bonusList.remove(bonus);
+                        RelativeLayout imageLayout = findViewById(R.id.imageLayout);
+                        imageLayout.removeView(bonusView);
+                    }
+                }
+
+            }
+
+            public void onFinish() {
+                finish = true;
+                startTimeBonus = System.currentTimeMillis();
+                Random r = new Random();
+                int x = 200 + r.nextInt(1500 - 200);
+                int y = 200 + r.nextInt(1500 - 200);
+
+                Bonus bonus = new Bonus(x, y, R.drawable.etoile, "life");
+                bonusList = new ArrayList<>();
+                bonusList.add(bonus);
+                RelativeLayout imageLayout = findViewById(R.id.imageLayout);
+                ImageView bonusView = new ImageView(context);
+                bonusView.setImageResource(bonus.getImage());
+                bonusView.setX(bonus.getX());
+                bonusView.setY(bonus.getY());
+                bonusView.setId(bonus.getId());
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(50, 50);
+                bonusView.setLayoutParams(layoutParams);
+                imageLayout.addView(bonusView);
+                this.start();
+            }
+        }.start();
+
+
+
     }
 
     public void goToresult(View view){
@@ -181,6 +245,16 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private boolean collision(View flyingman, View bonus)
+    {
+        boolean collision = false;
+        if (flyingman.getX() >= bonus.getX()-10  && flyingman.getX() <= bonus.getX()-10 && flyingman.getY() >= bonus.getY()-10  && flyingman.getY() <= bonus.getY()-10)
+        {
+            collision = true;
+        }
+        return collision;
     }
 
 }
