9fefbe2aebb6bf6466df8e877d940e11a4ff02b6
MontasserBH <montasserb94@gmail.com>
2018-03-16 11:27:05 +0100

    Regler activation de button start

--- app/src/main/java/com/m2dl/mobebmp/mobe_catapulte/StartActivity.java
+++ app/src/main/java/com/m2dl/mobebmp/mobe_catapulte/StartActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,6 +14,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class StartActivity extends AppCompatActivity {
 
     DatabaseReference databaseReference;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +23,21 @@ public class StartActivity extends AppCompatActivity {
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
-        final EditText name = findViewById(R.id.editText3);
+        name = findViewById(R.id.editText3);
         Button buttonStart = findViewById(R.id.button_start);
+    }
 
+    public void goToGameActivity(View view){
         String nom = name.getText().toString();
-        if (nom.isEmpty() || nom.equals(" ")){
-            buttonStart.setClickable(false);
+        if (nom.length() > 2){
+            Utilisateur utilisateur = new Utilisateur(name.toString(), 0);
+            databaseReference.child(name.getText().toString()).setValue(utilisateur);
+            Intent intent = new Intent(StartActivity.this, GameActivity.class);
+            intent.putExtra("nameFromStartActivity", name.getText().toString());
+            startActivity(intent);
         }
         else {
-            buttonStart.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Utilisateur utilisateur = new Utilisateur(name.toString(), 0);
-                    databaseReference.child(name.getText().toString()).setValue(utilisateur);
-                    Intent intent = new Intent(StartActivity.this, GameActivity.class);
-                    intent.putExtra("nameFromStartActivity", name.getText().toString());
-                    startActivity(intent);
-                }
-            });
+            Toast.makeText(this, "Nom trop court. Saisissez au moins 3 charactères", Toast.LENGTH_LONG).show();
         }
     }
 }
